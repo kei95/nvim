09f3853f81a5bdf587ea7b63c21ffe04e95566c8
kei95 <keisukey@wix.com>
2024-12-02 17:30:11 +0900

    Replace copilot with the one from official

--- README.md
+++ README.md
@@ -6,3 +6,8 @@
 ## Language Servers
 - Command `:Mason`
 - Select Items mentioned in the txt file
+
+## Copilot
+In order to use copilot, make sure to log into Github with an account with copilot enabled
+- Command `:Copilot setup`
+- Follow the instructions 
--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,18 +1,3 @@
 return {
-  "zbirenbaum/copilot.lua",
-  config = function()
-    require("copilot").setup({
-      suggestion = {
-        enabled = true,
-        auto_trigger = true,
-        keymap = {
-          accept = "<C-l>", -- Accept suggestion
-          next = "<C-j>", -- Next suggestion
-          prev = "<C-k>", -- Previous suggestion
-          dismiss = "<C-h>", -- Dismiss suggestion
-        },
-      },
-      panel = { enabled = true },
-    })
-  end,
+  'github/copilot.vim'
 }
