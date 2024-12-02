e5a60eedc51c66653ca9e1b74f0e7c753399a6be
kei95 <keisukey@wix.com>
2024-12-02 11:07:12 +0900

    Update launage setting and lock file

--- init.lua
+++ init.lua
@@ -1,4 +1,6 @@
 -- Bootstrap lazy.nvim
+vim.cmd("language en_US")
+
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
