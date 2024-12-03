ba7ee20a1f6269944b54fe8ac615bdf7af7f6c8d
kei95 <keisukey@wix.com>
2024-12-03 19:11:47 +0900

    Fix order of vim key bindings in vim-option and init file

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -4,5 +4,10 @@ vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
 vim.g.mapleader = " "
 vim.g.background = "light"
-
 vim.opt.swapfile = false
+vim.api.nvim_set_option("clipboard", "unnamed")
+-- To jump around splitted windows
+vim.keymap.set('n', '<leader>h', '<C-w>h', { noremap = true, silent = true }) -- Move to the left split
+vim.keymap.set('n', '<leader>j', '<C-w>j', { noremap = true, silent = true }) -- Move to the split below
+vim.keymap.set('n', '<leader>k', '<C-w>k', { noremap = true, silent = true }) -- Move to the split above
+vim.keymap.set('n', '<leader>l', '<C-w>l', { noremap = true, silent = true }) -- Move to the right split
