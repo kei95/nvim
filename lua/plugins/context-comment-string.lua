ccd276e0a9ed92393451030ea30604b49719f880
kei95 <keisukey@wix.com>
2024-12-05 23:33:48 +0900

    Fix context comment plug in

--- lua/plugins/context-comment-string.lua
+++ lua/plugins/context-comment-string.lua
@@ -1,3 +1,33 @@
 return {
-  "JoosepAlviste/nvim-ts-context-commentstring",
+	{
+		"JoosepAlviste/nvim-ts-context-commentstring",
+		lazy = false, -- Ensure it's loaded eagerly
+		config = function()
+			require("ts_context_commentstring").setup({
+				enable_autocmd = false, -- Disable CursorHold autocommand
+			})
+		end,
+	},
+	{
+		-- Ensure commenting functionality works seamlessly
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup({
+				pre_hook = function(ctx)
+					-- Use ts_context_commentstring for files where it's needed
+					if vim.bo.filetype == "javascript" or vim.bo.filetype == "typescriptreact" then
+						return require("ts_context_commentstring.internal").calculate_commentstring()
+					end
+				end,
+			})
+
+      -- Override get_option to use ts_context_commentstring's commentstring
+			local get_option = vim.filetype.get_option
+			vim.filetype.get_option = function(filetype, option)
+				return option == "commentstring"
+						and require("ts_context_commentstring.internal").calculate_commentstring()
+					or get_option(filetype, option)
+			end
+		end,
+	},
 }
