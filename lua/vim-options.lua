e978f0a9510de7a2b650464941758a8d8c090269
keisuke.yamashite <keisuke.yamashite@SFLM0091.local>
2024-12-14 10:38:27 +0900

    Add key map on esc for terminal mode

--- lua/vim-options.lua
+++ lua/vim-options.lua
@@ -2,10 +2,12 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+
 vim.g.mapleader = " "
 vim.g.background = "light"
 vim.opt.swapfile = false
 vim.api.nvim_set_option("clipboard", "unnamed")
+vim.api.nvim_set_keymap('t', '<Esc>', '<C-\\><C-n>', { noremap = true, silent = true })
 vim.o.encoding = "utf-8"
 vim.opt.fileencodings = { "utf-8" }
 
