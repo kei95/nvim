1480635dfb640c1e43f13b0c67681fcfea2b1fa0
kei95 <keisukey@wix.com>
2025-01-10 12:15:31 +0900

    Set minimalistic eslint to non-ls

--- lua/plugins/non-ls.lua
+++ lua/plugins/non-ls.lua
@@ -1,24 +1,33 @@
 return {
-  {
-    "nvimtools/none-ls.nvim",
-    dependencies = {
-      "nvimtools/none-ls-extras.nvim",
-    },
-    config = function()
-      local null_ls = require("null-ls")
+	{
+		"williamboman/mason.nvim",
+		config = true,
+	},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		dependencies = {
+			"nvimtools/none-ls.nvim",
+			"nvimtools/none-ls-extras.nvim",
+		},
+		config = function()
+			require("mason-null-ls").setup({
+				ensure_installed = { "eslint_d", "prettier", "stylua" }, -- Install these automatically
+				automatic_installation = true, -- Automatically install configured sources
+			})
 
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.stylua,
-          null_ls.builtins.formatting.prettier,
-          null_ls.builtins.completion.spell,
-        },
-      })
-      require("none-ls.diagnostics.eslint_d")
-      vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
-    end,
-  },
-  {
-    "nvimtools/none-ls-extras.nvim",
-  },
+			local null_ls = require("null-ls")
+
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.prettier,
+					null_ls.builtins.completion.spell,
+					require("none-ls.diagnostics.eslint_d"),
+					require("none-ls.formatting.eslint_d"),
+				},
+			})
+
+			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
+		end,
+	},
 }
