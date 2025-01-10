7d86ea3d3a44aa7c3ba994ec125ced42c850afbf
keisuke.yamashite <keisuke.yamashite@SFLM0091.local>
2025-01-11 05:18:07 +0900

    update: Enable hybrid mode in Volar

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -43,7 +43,7 @@ return {
       --   filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
       --   init_options = {
       --     vue = {
-      --       hybridMode = false,
+      --       hybridMode = true,
       --     },
       --     typescript = {
       --       tsdk =
