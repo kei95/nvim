f1082b2262b962d29b5946367cbe6fc446f0aafe
keisuke.yamashite <keisuke.yamashite@SFLM0091.local>
2025-01-24 00:44:51 +0900

    Fix mapping conflict in coplit and copilot chat

--- lua/plugins/copilotChat.lua
+++ lua/plugins/copilotChat.lua
@@ -8,6 +8,36 @@ return {
 		build = "make tiktoken", -- Only on MacOS or Linux
 		opts = {
 			-- See Configuration section for options
+			mappings = {
+				-- Use tab for completion
+				complete = {
+					insert = "", -- Use <Tab> for completion
+				},
+				-- Close the chat
+				close = {
+					normal = "q",
+					insert = "<C-c>",
+				},
+				-- Reset the chat buffer
+				reset = {
+					normal = "<C-x>",
+					insert = "<C-x>",
+				},
+				-- Submit the prompt to Copilot
+				submit_prompt = {
+					normal = "<CR>",
+					insert = "<C-CR>",
+				},
+				-- Accept the diff
+				accept_diff = {
+					normal = "<C-y>",
+					insert = "<C-y>",
+				},
+				-- Show help
+				show_help = {
+					normal = "g?",
+				},
+			},
 		},
 		config = function(_, opts)
 			-- Setup the plugin
