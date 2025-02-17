6703d8feedda67b2e6d7be470f41a37284dcc50a
kei95 <keisukey@wix.com>
2025-02-17 20:54:27 +0900

    Add binding for go reference

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -58,8 +58,9 @@ return {
 
 			-- Keymaps
 			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition (LSP)" })
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Find references (LSP)" })
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Open Code action (LSP)" })
 			vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { desc = "Rename symbol (LSP)" })
 			vim.keymap.set("n", "<C-k>", vim.diagnostic.open_float, { desc = "Peek LSP Error" })
 			vim.keymap.set("n", "n[", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
