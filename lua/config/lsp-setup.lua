791e58e7fb201b8614a11662f4de8874ec2733ba
kei95 <keisukey@wix.com>
2025-05-09 11:04:11 +0900

    Update how lspconfig gets set

---
+++ lua/config/lsp-setup.lua
@@ -0,0 +1,69 @@
+local lspconfig = require("lspconfig")
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+local function file_exists(name)
+	return vim.fn.filereadable(name) == 1
+end
+
+local function has_file_with_ext(ext)
+	return vim.fn.glob("**/*." .. ext) ~= ""
+end
+
+local function package_has(dep)
+	local path = "package.json"
+	if not file_exists(path) then
+		return false
+	end
+
+	local ok, json = pcall(vim.fn.json_decode, vim.fn.readfile(path))
+	if not ok or not json then
+		return false
+	end
+
+	local all_deps = vim.tbl_deep_extend("force", json.dependencies or {}, json.devDependencies or {})
+	return all_deps[dep] ~= nil
+end
+
+local function setup_lsp_per_project()
+	-- Always-enabled LSPs
+	lspconfig.lua_ls.setup({ capabilities = capabilities })
+	lspconfig.jsonls.setup({ capabilities = capabilities })
+	lspconfig.html.setup({ capabilities = capabilities })
+	lspconfig.eslint.setup({ capabilities = capabilities })
+
+	-- Vue project
+	if has_file_with_ext("vue") or package_has("vue") then
+		lspconfig.volar.setup({
+			capabilities = capabilities,
+			filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
+			init_options = {
+				typescript = {
+					tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib",
+				},
+			},
+		})
+		print("🔧 LSP: Volar enabled (Vue project)")
+		return
+	end
+
+	-- React project
+	if package_has("react") then
+		lspconfig.ts_ls.setup({
+			capabilities = capabilities,
+		})
+		print("🔧 LSP: ts_ls enabled (React project)")
+		return
+	end
+
+	-- Vanilla TS/JS
+	if has_file_with_ext("ts") or has_file_with_ext("js") then
+		lspconfig.ts_ls.setup({
+			capabilities = capabilities,
+		})
+		print("🔧 LSP: ts_ls enabled (Vanilla TS/JS project)")
+	end
+end
+
+return {
+	setup_lsp_per_project = setup_lsp_per_project,
+}
--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,70 +1,36 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		opts = {
-			auto_install = true,
-		},
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			local lspconfig = require("lspconfig")
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    config = function()
+      require("mason-lspconfig").setup({
+        automatic_installation = true,
+        automatic_enable = false,
+      })
+    end,
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      require("config.lsp-setup").setup_lsp_per_project()
 
-			-- ============================== Volar set up ==============================
-			-- Path to the root-level TypeScript installation
-			-- local root_tsdk = vim.fn.getcwd() .. "/node_modules/typescript/lib"
-			--
-			-- lspconfig.volar.setup({
-			-- 	capabilities = capabilities,
-			-- 	filetypes = { "typescript", "javascript", "javascriptreact", "typescriptreact", "vue" },
-			-- 	init_options = {
-			-- 		typescript = {
-			-- 			tsdk = root_tsdk, -- Set the path to the root TypeScript SDK
-			-- 		},
-			-- 		vue = {
-			-- 			hybridMode = false,
-			-- 		},
-			-- 	},
-			-- })
-			-- ============================================================
-
-			-- Freaking annoying, but when workig on non-Vue project, I need to comment out ts_ls and uncomment the Volar
-			lspconfig.ts_ls.setup({
-				capabilities = capabilities,
-			})
-			-- Language server setups
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.html.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.jsonls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.eslint.setup({
-				capabilities = capabilities,
-			})
-
-			-- Keymaps
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition (LSP)" })
-			vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Find references (LSP)" })
-			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Open Code action (LSP)" })
-			vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { desc = "Rename symbol (LSP)" })
-			vim.keymap.set("n", "<C-k>", vim.diagnostic.open_float, { desc = "Peek LSP Error" })
-			vim.keymap.set("n", "n[", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
-			vim.keymap.set("n", "n]", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
-		end,
-	},
+      -- LSP-related keymaps
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition (LSP)" })
+      vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Find references (LSP)" })
+      vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, { desc = "Open Code action (LSP)" })
+      vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, { desc = "Rename symbol (LSP)" })
+      vim.keymap.set("n", "<C-k>", vim.diagnostic.open_float, { desc = "Peek LSP Error" })
+      vim.keymap.set("n", "n[", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
+      vim.keymap.set("n", "n]", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
+    end,
+  },
 }
