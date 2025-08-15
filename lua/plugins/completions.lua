0dae62f6f3d35613e4dc5be483c5f3eabd96d559
kei95 <keisukey@wix.com>
2025-08-15 15:44:26 +0900

    Fix lua lsp buffer error

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -1,53 +1,14 @@
 return {
-  {
-    "hrsh7th/cmp-nvim-lsp"
-  },
-  {
-    "L3MON4D3/LuaSnip",
-    dependencies = {
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-    },
-  },
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-buffer", -- make sure buffer source is properly installed
-    },
-    config = function()
-      local cmp = require("cmp")
-      require("luasnip.loaders.from_vscode").lazy_load()
-
-      cmp.setup({
-        snippet = {
-          expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-          end,
-        },
-        window = {
-          completion = cmp.config.window.bordered(),
-          documentation = cmp.config.window.bordered(),
-        },
-        mapping = cmp.mapping.preset.insert({
-          ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-f>"] = cmp.mapping.scroll_docs(4),
-          ["<C-Space>"] = cmp.mapping.complete(),
-          ["<C-e>"] = cmp.mapping.abort(),
-          ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        }),
-        sources = cmp.config.sources({
-          { name = "nvim_lsp" },
-          { name = "luasnip" },
-          -- Temporarily disable the buffer source to avoid to_utfindex issue
-          -- { name = "buffer" },
-        }),
-        formatting = {
-          format = function(entry, vim_item)
-            -- Defensive formatting to avoid invalid entries causing issues
-            return vim_item
-          end,
-        },
-      })
-    end,
-  },
+	-- LSP source
+	{ "hrsh7th/cmp-nvim-lsp", version = false },
+  -- Snippets
+	{
+		"L3MON4D3/LuaSnip",
+		dependencies = {
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+		},
+	},
+	-- Core completion
+	{ "hrsh7th/cmp-nvim-lsp", version = false },
 }
