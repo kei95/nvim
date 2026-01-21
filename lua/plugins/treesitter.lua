9b1f967e8f45c5edcd06edab20d8189c6437de67
kei95 <kei0613shine.star@gmail.com>
2026-01-22 02:12:11 +0900

    Remove config check on treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,12 +3,14 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-      local config = require("nvim-treesitter.configs")
-      config.setup({
-        ensure_installed = { "markdown", "markdown_inline" },
-        auto_install = true,
-        highlight = { enable = true },
-        indent = { enable = true },
+      require("nvim-treesitter").setup({})
+      -- Install parsers
+      require("nvim-treesitter").install({ "markdown", "markdown_inline" })
+      -- Enable treesitter highlighting for all buffers
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          pcall(vim.treesitter.start)
+        end,
       })
     end,
   },
